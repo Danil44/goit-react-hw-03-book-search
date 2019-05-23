327281d934e81fb217a8c5b4f3725e76c12959e9
Daniil Golovenko (CODER) <d.golovenko@itcg.ua>
2019-05-23 13:18:17 +0300

    Updates

---
+++ precache-manifest.152640008e74b637c5280b04067edd93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94506de2c381820c5c24",
+    "url": "/goit-react-hw-03-book-search/static/js/runtime~main.5d5bdee0.js"
+  },
+  {
+    "revision": "2cffcdf152934af90e10",
+    "url": "/goit-react-hw-03-book-search/static/js/main.4b48f0c3.chunk.js"
+  },
+  {
+    "revision": "0259fbc5b3a06bc88893",
+    "url": "/goit-react-hw-03-book-search/static/js/2.744a21fd.chunk.js"
+  },
+  {
+    "revision": "2cffcdf152934af90e10",
+    "url": "/goit-react-hw-03-book-search/static/css/main.9709a59d.chunk.css"
+  },
+  {
+    "revision": "6e47c30fd4f732e7db8605aaf8624229",
+    "url": "/goit-react-hw-03-book-search/index.html"
+  }
+];
